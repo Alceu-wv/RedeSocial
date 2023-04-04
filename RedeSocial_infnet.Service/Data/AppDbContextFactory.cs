70d690b1366668e832325c6b218a17e8a55fafe8
Alceu-wv <alceu.wv@gmail.com>
2023-04-04 08:58:31 -0300

    update DB

--- RedeSocial_infnet.Service/Data/AppDbContextFactory.cs
+++ RedeSocial_infnet.Service/Data/AppDbContextFactory.cs
@@ -14,7 +14,7 @@ namespace RedeSocial_infnet.Service.Data
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Server = tcp:booksmanagerserver.database.windows.net,1433; Initial Catalog = BooksManagerDB; Persist Security Info = False; User ID = adminserver; Password ={ your_password}; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+            optionsBuilder.UseSqlServer("Server = tcp:booksmanagerserver.database.windows.net,1433; Initial Catalog = rede_social_infnet; Persist Security Info = False; User ID = adminserver; Password = Aulainfnet123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
             //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=rede_social_infnet;Trusted_Connection=True");
             return new AppDbContext(optionsBuilder.Options);
         }
